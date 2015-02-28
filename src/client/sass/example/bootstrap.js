b3848e243173c09a9bfe18cac4f8ff59a81f37da
aerhard <a_erhard@gmx.de>
2015-02-28 13:52:17 +0100

    added image overrides

--- src/client/sass/example/bootstrap.js
+++ src/client/sass/example/bootstrap.js
@@ -11,6 +11,7 @@ Ext.Loader.addClassPathMappings({
   "Ext.EventObjectImpl": "../../ext/src/EventObject.js",
   "Ext.Msg": "../../ext/src/window/MessageBox.js",
   "Ext.Supports": "../../ext/src/Support.js",
+  "Ext.cmd": "../../../../../../../bin/Sencha/Cmd/5.1.2.52/plugins/src",
   "Ext.core.DomHelper": "../../ext/src/dom/Helper.js",
   "Ext.core.DomQuery": "../../ext/src/dom/Query.js",
   "Ext.locale.de.Component": "../../ext/locale/ext-lang-de.js",
